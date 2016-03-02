b5a797b12fe4e156d061ebcc76cf178eee0b682f
franckyy <franck.calestroupat@gmail.com>
2016-03-02 18:22:22 +0100

    La sauvegarde d'un client fonctionne ! Reste à mapper correctement car le numéro de voie ne remonte pas dans la table.

--- commandes/src/main/webapp/js/gestion_clients.js
+++ commandes/src/main/webapp/js/gestion_clients.js
@@ -15,37 +15,17 @@ chezYenApp.controller("clientCtrl", function($scope, $http) {
    $scope.nouveau_client = function(nom, prenom, numVoie, typeVoie, nomVoie, codePostal, ville){
 	   console.log("nouveau client");
 	   $http.post('../gestClients/nouveau', {
-		   	"client_nom": nom,
-		   	"client_prenom": prenom,
-		   	"client_numVoie": numVoie,
-		   	"client_typeVoie": typeVoie,
-		   	"client_nomVoie": nomVoie,
-		   	"client_codePostal": codePostal,
-		   	"client_ville": ville
+		   	"clientNom": nom,
+		   	"clientPrenom": prenom,
+		   	"clientNumeroVoie": numVoie,
+		   	"clientTypeVoie": typeVoie,
+		   	"clientNomVoie": nomVoie,
+		   	"clientCodePostal": codePostal,
+		   	"clientVille": ville
 		   }).success(function(response) {
 			   $scope.clients.push(response.client);
 	   });
    };
-//   $scope.incompleteCount = function() {
-//     var count = 0;
-//     angular.forEach($scope.produits, function(t) {
-//       if (!t.completed) {
-//           count++;
-//       }  
-//     });
-//     return count;
-//   };
-   
-//   $scope.warningLevel = function() {
-//     if ($scope.incompleteCount() < 3 ) {
-//        return "label-success";  
-//     }
-//     else {
-//        return "label-warning";
-//     }
-//   };
-   
-
 // $scope.addNewTask2 = function(libelle, category) {
 //   // j'envoie une requette POST pour demander au serveur de creer la tache en BDD
 //   $http.post('../rest/savetache2', {"tacheLibelle": libelle, "tacheCategory" : category} 
@@ -57,42 +37,31 @@ chezYenApp.controller("clientCtrl", function($scope, $http) {
 //       $scope.taches.push(response.tache);
 //    }); 
 // };
-   
-   // appelé quand le formulaire est soumis (click bouton)
-   // on recoit le libelle de la tache a cree, et sa categorie
-//   $scope.nouveauClient = function(nom, prenom, adresse) {
-//     // j'envoie une requette POST pour demander au serveur de creer la tache en BDD
-//     $http.post('../rest/saveclient/' + nom + '/' + prenom + '/' + adresse, /* ce client sera créé avec les parametres ci dessous*/ 
-//      {}).success(function(response) {
-//          // cette fonction est rappellée quand le serveur renvoie sa réponse
-//          // le serveur nous renvoie le client inséré sous format JSON
-//          // angular désérialisé le json dans response.data
-//          // nous ajoutons le client inséré dans le tableau des clients du scope
-//         $scope.clients.push(response.client);
-//      }); 
-//   };
 //   
 //   $scope.deleteClient = function(id) {
 //       console.log("delete client no " + id);
 //   };
 
-
-
-   
-   /*
-   $scope.addNewTask = function(libelle, category) {
-     var tid =   $scope.taches.length + 1;
-     var tdate = "" + new Date();
-     $scope.taches.push({
-       "libelle" : libelle,
-       "category" : category,
-       "completed" : false,
-       "id" : tid,
-       "dateCreated" : tdate 
-     });
-   };
-   */
 });
+
+//$scope.incompleteCount = function() {
+//var count = 0;
+//angular.forEach($scope.produits, function(t) {
+//  if (!t.completed) {
+//      count++;
+//  }  
+//});
+//return count;
+//};
+
+//$scope.warningLevel = function() {
+	//if ($scope.incompleteCount() < 3 ) {
+	//   return "label-success";  
+	//}
+	//else {
+	//   return "label-warning";
+	//}
+//};
 
 // filter prend en parametre le nom du filtre, et la fonction
 // "factory" fabrique de la fonction filtre
