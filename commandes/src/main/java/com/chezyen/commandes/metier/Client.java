a5b90ceeb0778c0d29833e949a2f161134c9f7b1
franckymint <franckymint@franckymint-MS-7816>
2016-07-17 11:56:37 +0200

    Ajout des contacts dans la création du client (gestion_client.html) Ajout des attributs telephone et email dans la classe Client.java -> mise à jour des constructeurs et de la méthode setClient()

--- commandes/src/main/java/com/chezyen/commandes/metier/Client.java
+++ commandes/src/main/java/com/chezyen/commandes/metier/Client.java
@@ -21,25 +21,30 @@ public class Client implements IGenericEntity {
 	private String nom;
 	private String prenom;
 	private String email;
+	private String telephone;
 	@Embedded
 	private Adresse adresse;
 	@OneToMany(mappedBy="client")
 	private Set<Commande> commandes;
 	
 	public Client() {super();}
-	public Client(String nom, String prenom, String email, Adresse adresse) {
-		this(nom, prenom, email, adresse, null);
+	//Constructeur sans la commande
+	public Client(String nom, String prenom, String email, String telephone, Adresse adresse) {
+		this(nom, prenom, email, telephone, adresse, null);
 	}
-	public Client(int idClient, String nom, String prenom, String email, Adresse adresse) {
-		this(nom, prenom, email, adresse, null);
+	//Constructeur sans la commande mais avec un identifiant
+	public Client(int idClient, String nom, String prenom, String email, String telephone, Adresse adresse) {
+		this(nom, prenom, email, telephone, adresse, null);
 		this.idClient = idClient;
 	}
-	public Client(String nom, String prenom, String email, Adresse adresse, Set<Commande> commandes) {
+	//Constructeur complet mais sans identifiant
+	public Client(String nom, String prenom, String email, String telephone, Adresse adresse, Set<Commande> commandes) {
 		super();
 		log.info("new Client() - nom : " + nom);
 		this.nom = nom;
 		this.prenom = prenom;
 		this.email = email;
+		this.telephone = telephone;
 		this.adresse = adresse;
 		this.commandes = commandes;
 	}
@@ -53,6 +58,8 @@ public class Client implements IGenericEntity {
 	public void setPrenom(String prenom) {this.prenom = prenom;}
 	public String getEmail() {return email;}
 	public void setEmail(String email) {this.email = email;}
+	public String getTelephone() {return telephone;}
+	public void setTelephone(String telephone) {this.telephone = telephone;}
 	public Adresse getAdresse() {return adresse;}
 	public void setAdresse(Adresse adresse) {this.adresse = adresse;}
 	public Set<Commande> getCommandes() {return commandes;}
@@ -64,9 +71,9 @@ public class Client implements IGenericEntity {
 		return this.getIdClient();
 	}
 	
-	public Client setClient(int id, String nom, String prenom, String email, String nomVoie, String typeVoie, String numeroVoie, int codePostal, String ville) {
+	public Client setClient(int id, String nom, String prenom, String telephone, String email, String nomVoie, String typeVoie, String numeroVoie, int codePostal, String ville) {
 		Adresse adresse = new Adresse(nomVoie, typeVoie, numeroVoie, codePostal, ville);
-		Client client = new Client(id, nom, prenom, email, adresse);
+		Client client = new Client(id, nom, prenom, email, telephone, adresse);
 		return client;
 	}
 }
