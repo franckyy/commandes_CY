57bc471525e33b2cc859476452c83fb7ea0cbf2d
franckymint <franckymint@franckymint-MS-7816>
2016-08-06 17:01:22 +0200

    travail sur le choix du produit dans la création d'un produit conditionné

--- commandes/src/main/webapp/js/gestion_produits_conditionnes.js
+++ commandes/src/main/webapp/js/gestion_produits_conditionnes.js
@@ -3,12 +3,16 @@ var chezYenApp = angular.module("chezYenApp", []);
 chezYenApp.controller("produitsConditionnesCtrl", function($scope, $http){
 	$scope.produitsConditionnes = [];
 	$scope.listeProduits  = [];
+	$scope.myProduit = [];	//par défaut, le premier produit sera affiché.
 	$scope.listeConditionnements = [];
+	$scope.myConditionnement = [];	//par défaut, le premier conditionnement sera affiché.
 	$scope.produitConditionneModif;
 	
 	$http.get('../gestProduitsConditionnes/repertoire').then (function(response) {
 		$scope.produitsConditionnes = response.data.produitsConditionnes;
 		$scope.listeProduits = response.data.listeProduits;
+		$scope.myProduit = $scope.listeProduits[0];
 		$scope.listeConditionnements = response.data.listeConditionnements;
+		$scope.myConditionnement = $scope.listeConditionnements[0];
 	});
 });
